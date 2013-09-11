b4c83d047a82c0bdbaaadef83dfd0a3fad9be3fc
Mike Karolow <mike@yesware.com>
2013-09-11 16:03:35 -0400

    Remove bash_it from homebrew recipe

--- sprout-osx-base/recipes/homebrew.rb
+++ sprout-osx-base/recipes/homebrew.rb
@@ -1,7 +1,6 @@
 return unless node["platform"] == "mac_os_x"
 
 include_recipe "sprout-osx-base::user_owns_usr_local"
-include_recipe "sprout-osx-base::bash_it"
 
 # Do not be tempted to use the git-resource to check out
 # homebrew directly into /usr/local; it will fail if
